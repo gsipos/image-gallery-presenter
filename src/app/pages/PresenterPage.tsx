219623522810ac0d0a80dcbf585314946616d56d
Gergely Sipos <gergely.sipos@aliz.ai>
2024-02-01 01:22:20 +0100

    sort images by size

--- src/app/pages/PresenterPage.tsx
+++ src/app/pages/PresenterPage.tsx
@@ -1,10 +1,13 @@
 import { BentoGrid } from '../atoms/BentoGrid'
+import { bentoSizes } from '../atoms/types'
 import { useSavedImages } from '../hooks/use-saved-images'
 import './PresenterPage.css'
 
 export const PresenterPage = () => {
   const { images } = useSavedImages()
-  const enabledImages = images.filter((image) => image.enabled)
+    const enabledImages = images.filter((image) => image.enabled)
+    const reverseBensoSizes = bentoSizes.toReversed()
+    enabledImages.sort((a, b) => bentoSizes.indexOf(b.size)- bentoSizes.indexOf(a.size))
   return (
     <div className="presenter-root">
       <BentoGrid>
